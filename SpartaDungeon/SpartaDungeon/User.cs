3f62fbe498c9f5053ea344ef9e68e53e134743da
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2024-04-22 15:00:20 +0900

    item, user 클래스 생성

---
+++ SpartaDungeon/SpartaDungeon/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class User
+{
+    public int Level;
+    public int Gold;
+
+    public string Name;
+    public string Job;
+
+    public float AttackPower;
+    public float Defense;
+    public float HP;
+
+    public User(string name, int level, string job, int attackPower, int defense, int hp, int gold)
+	{
+        Name = name;    
+        Level = level;
+        Job = job;
+        AttackPower = attackPower;
+        Defense = defense;
+        HP = hp;
+        Gold = gold;
+    }
+}
