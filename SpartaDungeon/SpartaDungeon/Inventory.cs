e2b2f0974ae60614a65d84c204c7a737725df7d7
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2024-04-23 18:52:01 +0900

    인벤토리 - 장착관리 구현

--- SpartaDungeon/SpartaDungeon/Inventory.cs
+++ SpartaDungeon/SpartaDungeon/Inventory.cs
@@ -2,13 +2,19 @@
 {
     public class Inventory
     {
+        // 사용자의 아이템 리스트 선언 (가변적이어야 하므로 리스트 사용)
+        static List<Item> InventoryItems = new List<Item>();
+
         // 인벤토리
         public static void ShowInventory()
         {
             Console.WriteLine("\n--------------------------------------------------------------\n");
             Console.WriteLine("인벤토리\n");
             Console.WriteLine("[아이템 목록]\n");
-            Console.WriteLine("1. 장착 관리");
+
+            ShowInventoryItem();
+
+            Console.WriteLine("\n1. 장착 관리");
             Console.WriteLine("0. 나가기\n");
 
             while (true)
@@ -25,7 +31,80 @@
                 // 장착 관리
                 else if (input == 1)
                 {
+                    ManageEquipment();
+                }
+                else
+                {
+                    Console.WriteLine("잘못된 입력입니다.");
+                }
+            }
+        }
+
+        public static void AddItemInInventory(Item item)
+        {
+            InventoryItems.Add(item);
+        }
+
+        public static void ShowInventoryItem()
+        {
+            // 인벤토리에 아이템이 없으면
+            if (InventoryItems.Count == 0) { Console.WriteLine("인벤토리에 아이템이 없습니다. \n"); }
+            for (int i = 0; i < InventoryItems.Count; i++)
+            {
+                // 아이템의 속성 값이 0 이 아닌 것 모두 뽑기
+                List<string> itemProperty = new List<string>();
+                if (InventoryItems[i].AttackPower != 0) { itemProperty.Add(("공격력 +" + InventoryItems[i].AttackPower)); }
+                else if (InventoryItems[i].Defense != 0) { itemProperty.Add(("방어력 +" + InventoryItems[i].Defense)); }
+                else if (InventoryItems[i].HP != 0) { itemProperty.Add(("체력 +" + InventoryItems[i].HP)); }
+
+                Console.Write( " - " + (i+1) + " ");
+                // 장착 중인 아이템 앞에는 [E] 표시
+                if (InventoryItems[i].IsEquipped)
+                {
+                    Console.Write("[E] ");
+                }
+                Console.Write(InventoryItems[i].Name + "  |   ");
+                // 아이템 속성 
+                foreach (string property in itemProperty) { Console.Write(property + "  |   "); }
+                Console.WriteLine(InventoryItems[i].Description);
+            }
+        }
+
+        public static void ManageEquipment()
+        {
+            Console.WriteLine("\n--------------------------------------------------------------\n");
+            Console.WriteLine("인벤토리 - 장착관리\n");
+            Console.WriteLine("[아이템 목록]\n");
+
+            ShowInventoryItem();
+            Console.WriteLine("\n0. 나가기\n");
+
+            while (true)
+            {
+                Console.WriteLine("장착하고 싶거나 장착 해제하고 싶은 장비의 번호를 입력해주세요.");
+                int input = int.Parse(Console.ReadLine());
+
+                // 나가기
+                if (input == 0)
+                {
+                    ShowInventory();
                     break;
+                }
+                // 현재 인벤토리에 있는 물품의 숫자를 고른다면
+                else if (input <= InventoryItems.Count)
+                {
+                    if (InventoryItems[input - 1].IsEquipped) 
+                    {
+                        Console.WriteLine("장비의 장착을 해제합니다.");
+                        InventoryItems[input-1].IsEquipped = false;
+                        ManageEquipment();
+                    }
+                    else
+                    {
+                        Console.WriteLine("장비를 장착합니다.");
+                        InventoryItems[input - 1].IsEquipped = true;
+                        ManageEquipment();
+                    }
                 }
                 else
                 {
